c911e9e351e3b94f6ebec372d0940ff70f713bc2
tamiressg <t.graziele@escolar.ifrn.edu.br>
2026-07-14 22:07:32 -0300

    Update Interruptor.jsx

--- Interruptor.jsx
+++ Interruptor.jsx
@@ -1,5 +1,15 @@
-function Perfil({ nome }) {
-  return <p>Olá, {nome}!</p>;
+
+export default Perfil;import { useState } from 'react';
+
+function Interruptor() {
+  const [ligado, setLigado] = useState(false);
+
+  return (
+    <div style={{ margin: '10px 0' }}>
+      <p>O interruptor está: <strong>{ligado ? 'Ligado' : 'Desligado'}</strong></p>
+      <button onClick={() => setLigado(!ligado)}>Alternar</button>
+    </div>
+  );
 }
 
-export default Perfil;
+export default Interruptor;
