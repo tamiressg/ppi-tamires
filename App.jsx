853c18a206d7cf9055d6b4eda6a1a30124d3d777
tamiressg <t.graziele@escolar.ifrn.edu.br>
2026-07-14 22:08:04 -0300

    Update App.jsx

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+function Interruptor() {
+  const [ligado, setLigado] = useState(false);
+
+  return (
+    <div style={{ margin: '10px 0' }}>
+      <p>O interruptor está: <strong>{ligado ? 'Ligado' : 'Desligado'}</strong></p>
+      <button onClick={() => setLigado(!ligado)}>Alternar</button>
+    </div>
+  );
+}
+
+export default Interruptor;
